5916a67063ee35bb1040521c9733356e087ea9af
Adam Patterson <adam@farar.net>
2025-04-19 18:19:09 -0400

    more

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as https from "node:https";
 import * as fs from "node:fs";
 import * as ws from "ws";
 import { once } from "node:events";
-import { TelnyxController, DeepgramSTT, CartesiaTTS, OpenAIAgent, Dialogue, WebSocketEvents, log, SyslogLevel } from "@farar/nexus";
-import { EventEmitter } from "node:events";
+import { TelnyxController, DeepgramSTT, CartesiaTTS, OpenAIAgent, Dialogue, log, SyslogLevel, VoIP } from "@farar/nexus";
+
+log.setLevel(SyslogLevel.NOTICE);
 
 const {
   TELNYX_API_KEY = "",
@@ -31,7 +31,7 @@ httpServer.listen(parseInt(PORT.toString()), HOST_NAME);
 
 await once(httpServer, "listening");
 
-log.info(`httpServer is listening on ${PORT.toString()}, ${HOST_NAME}`);
+log.notice(`httpServer is listening on ${PORT.toString()}, ${HOST_NAME}`);
 
 const webSocketServer = new ws.WebSocketServer({ noServer: true });
 
@@ -42,10 +42,10 @@ const controller = new TelnyxController({
   streamURL: STREAM_URL
 });
 
-controller.on("init", (emitter: EventEmitter<WebSocketEvents>) => {
+controller.on("init", (voip: VoIP) => {  
   const stt = new DeepgramSTT({ apiKey: DEEPGRAM_API_KEY, openAIAPIKey: OPENAI_API_KEY });
   const tts = new CartesiaTTS({ apiKey: CARTESIA_API_KEY });
   const agent = new OpenAIAgent({ apiKey: OPENAI_API_KEY, system: OPENAI_SYSTEM_MESSAGE, greeting: OPENAI_GREETING_MESSAGE });
-
-  const dialogue = new Dialogue({ stt, tts, agent, emitter });
+  const dialogue = new Dialogue({ voip, stt, tts, agent });
+  dialogue.start();
 });
